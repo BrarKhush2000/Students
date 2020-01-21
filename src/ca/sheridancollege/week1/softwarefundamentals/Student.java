25cafc1a036f043c00902be03d03d36d863a1a0f
BrarKhush2000 <brar326@sheridanc.on.ca>
2020-01-21 17:12:32 -0500

    6th Commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,6 +12,7 @@ public class Student {
     private String city;
     private String grade;
     private String Status;
+    private String Books;
 
     public String getGrade() {
         return grade;
